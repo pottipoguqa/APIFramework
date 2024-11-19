e7143ddc98aedfdb8a8598e90cde17bc0e1072e0
pottipoguqa <pottipoguqa23@gmail.com>
2024-11-19 16:25:46 +0530

    updated by Eclipse

--- src/test/java/JavaTest1.java
+++ src/test/java/JavaTest1.java
@@ -1,8 +1,6 @@
 public class JavaTest1 {
     public static void main(String[]args){
         System.out.println("updated by Tester1");
-        System.out.println("updated by Tester1");
-        System.out.println("updated by Tester1");
         System.out.println("updated by Tester2");
         
     }
